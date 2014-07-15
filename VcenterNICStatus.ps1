52c6c88d7c56fa9a78ebb9fa55d2312aaf93e60a
claytob <barry.clayton@melbourne.co.uk>
2014-07-15 15:23:24 +0100

    vmware

---
+++ VcenterNICStatus.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\prtgshell-master\prtgshell.psm1"
+Add-PsSnapin VMware.VimAutomation.Core -ea "SilentlyContinue"
+#$Mycred = Get-Credential
+Connect-VIServer vcenter.infra.melbourne.co.uk -User prtg@system-domain -Password AK5mBFSnwu+R -WarningAction Ignore |Out-Null
+#Disconnect-VIServer vcenter
+
+$Result=Get-VMHostNetworkAdapter -physical |  where {$_.vmhost -like "*.infra.melbourne.co.uk" -and $_.bitratepersec -EQ 0} 
+
+$xmlOutput=""
+$xmlOutput="<prtg>"
+
+if ($result -eq "")
+{ $xmloutput += Set-PrtgResult "Number of NIC Uplinks in DOWN State"     0 Count -ShowChart}
+else
+{ $xmlOutput+= Set-PrtgResult "Number of NIC Uplinks in DOWN State"    $result.count  Count -ShowChart}
+
+$xmlOUtput +="</prtg>"
+#$xmlOUtput += set-prtgresult "Free Space GB" 22 sec
+#Write-Host   $TotalCapacityGB
+return $xmlOUtput
---
+++ VcenterStats.ps1
@@ -0,0 +1,40 @@
+﻿param
+(
+$VCenterServer,
+$User,
+$Password)
+
+Import-Module "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\prtgshell-master\prtgshell.psm1"
+Add-PsSnapin VMware.VimAutomation.Core -ea "SilentlyContinue"
+#$Mycred = Get-Credential
+
+#$Mycred = Get-Credential
+Connect-VIServer vcenter.infra.melbourne.co.uk -User prtg@system-domain -Password AK5mBFSnwu+R -WarningAction Ignore |Out-Null
+#Disconnect-VIServer vcenter
+
+
+
+$TotalCapacityGB=0
+$TotalFreeSpaceGB=0
+$xmlOutput=""
+
+$xmlOutput+="<prtg>"
+
+foreach( $Store in Get-Datastore | where {$_.name -like "ultravmclustervolume*"})
+{
+$xmlOutput += Set-PrtgResult "$($store.name) Free Space"     $(($store.FreeSpaceGB/$store.CapacityGB)*100) % -ShowChart
+#write-host "$($Store.Name) capcity: $($store.CapacityGB)"
+$TotalCapacityGB+= $store.CapacityGB
+$TotalFreeSpaceGB+=$Store.FreeSpaceGB
+}
+
+$xmlOUtput+=Set-PrtgResult "Overall Free Space" $(($TotalFreeSpaceGB/$TotalCapacityGB)*100) % -ShowChart
+$xmlOUtput+=Set-PrtgResult "Total Capacity GB" $TotalCapacityGB GB
+#$TotalCapacityGB= $store.CapacityGB
+
+
+
+$xmlOUtput +="</prtg>"
+#$xmlOUtput += set-prtgresult "Free Space GB" 22 sec
+#Write-Host   $TotalCapacityGB
+return $xmlOUtput
